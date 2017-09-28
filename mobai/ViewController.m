92ddf209feb462912e837699eb8919a8ddc23616
Miss-TDQ <2497877627@qq.com>
2017-09-28 12:56:37 +0800

    Initial Commit

---
+++ mobai/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  mobai
+//
+//  Created by miss on 2017/9/28.
+//  Copyright © 2017年 miss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ mobai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mobai
+//
+//  Created by miss on 2017/9/28.
+//  Copyright © 2017年 miss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
